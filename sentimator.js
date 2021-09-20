ca3c1732a3a93a6fcd7671eddd1c070a76cfa18e
zivserphos <zivfromisrael@gmail.com>
2021-09-20 23:58:23 +0300

    async function sentimator()
    right now the function print in the console the result of the text that he recived

--- sentimator.js
+++ sentimator.js
@@ -1,12 +1,31 @@
 
 document.body.append("suck my cock")
 
-
-async function sentimaor(text)
+let x;
+async function sentimator()
 {
-    const response = await fetch("https://sentim-api.herokuapp.com/")
-    const toJson = await response.json()
-    return toJson
-}
+    const response = await fetch("https://sentim-api.herokuapp.com/api/v1/" ,
+    {
+        method: "POST",
+        headers:
+        {
+            Accept: "application/json", "Content-Type": "application/json"      
+        } ,
+        body: JSON.stringify({ "text": x })
+    })
 
-document.body.append(await sentimaor())
+    if (response.ok)
+    {
+        console.log(await response.json())
+    }
+    else
+    {
+        alert("your mom's a hoe")
+    }
+    
+}
+document.getElementById("submit").addEventListener("click", (e) => {
+x = document.getElementById("text").value
+sentimator()
+})
+
